3d778f9219a0a0918466cdeb6a91b40ee06b3360
Damian Mooyman <damian@silverstripe.com>
2014-08-27 17:11:48 +1200

    BUG Fix missed due dates being forgotten Added tests and .travis.yml

--- code/controllers/CronTaskController.php
+++ code/controllers/CronTaskController.php
@@ -8,13 +8,20 @@
 class CronTaskController extends Controller {
 
 	/**
-	 * Only allow access to the index method
+	 * If this controller is in quiet mode
 	 *
-	 * @var array
+	 * @var bool
 	 */
-	private static $allowed_actions = array(
-		'index'
-	);
+	protected $quiet = false;
+
+	/**
+	 * Tell the controller how noisy it may be
+	 *
+	 * @param bool $quiet If set to true this controller will not emit debug noise
+	 */
+	public function setQuiet($quiet) {
+		$this->quiet = true;
+	}
 
 	/**
 	 * Checks for cli or admin permissions and include the library
@@ -31,7 +38,7 @@ class CronTaskController extends Controller {
 			if(!class_exists('Cron\CronExpression')) {
 				throw new Exception('CronExpression library isn\'t loaded, please see crontask README');
 			}
-		};
+		}
 
 		// Unless called from the command line, we need ADMIN privileges
 		if(!Director::is_cli() && !Permission::check("ADMIN")) {
@@ -40,33 +47,79 @@ class CronTaskController extends Controller {
 	}
 
 	/**
+	 * Determine if a task should be run
+	 *
+	 * @param CronTask $task
+	 * @param \Cron\CronExpression $cron
+	 */
+	public function isTaskDue(CronTask $task, \Cron\CronExpression $cron) {
+		// Get last run status
+		$status = CronTaskStatus::get_status(get_class($task));
+		
+		// If the cron is due immediately, then run it
+		$now = new DateTime(SS_Datetime::now()->getValue());
+		if($cron->isDue($now)) {
+			if(empty($status) || empty($status->LastRun)) return true;
+			// In case this process is invoked twice in one minute, supress subsequent executions
+			$lastRun = new DateTime($status->LastRun);
+			return $lastRun->format('Y-m-d H:i') != $now->format('Y-m-d H:i');
+		}
+
+		// If this is the first time this task is ever checked, no way to detect postponed execution
+		if(empty($status) || empty($status->LastChecked)) return false;
+
+		// Determine if we have passed the last expected run time
+		$nextExpectedDate = $cron->getNextRunDate($status->LastChecked);
+		return $nextExpectedDate <= $now;
+	}
+
+	/**
 	 * Default controller action
 	 *
-	 * @param object  $request
+	 * @param SS_HTTPRequest $request
 	 */
 	public function index(SS_HTTPRequest $request) {
-		foreach(ClassInfo::implementorsOf('CronTask') as $subclass) {
+		// Check each task
+		$tasks = ClassInfo::implementorsOf('CronTask');
+		if(empty($tasks)) {
+			$this->output("There are no implementators of CronTask to run");
+			return;
+		}
+		foreach($tasks as $subclass) {
 			$task = new $subclass();
-			$cron = Cron\CronExpression::factory($task->getSchedule());
-			if($cron->isDue()) {
-				$this->output($subclass.' will start now.');
-				$task->process();
-				continue;
-			}
-			$this->output($subclass.' will run at '.$cron->getNextRunDate()->format('Y-m-d H:i:s').'.');
+			$this->runTask($task);
+		}
+	}
+
+	/**
+	 * Checks and runs a single CronTask
+	 *
+	 * @param CronTask $task
+	 */
+	public function runTask(CronTask $task) {
+		$cron = Cron\CronExpression::factory($task->getSchedule());
+		$isDue = $this->isTaskDue($task, $cron);
+		// Update status of this task prior to execution in case of interruption
+		CronTaskStatus::update_status(get_class($task), $isDue);
+		if($isDue) {
+			$this->output(get_class($task).' will start now.');
+			$task->process();
+		} else {
+			$this->output(get_class($task).' will run at '.$cron->getNextRunDate()->format('Y-m-d H:i:s').'.');
 		}
 	}
 
 	/**
 	 * Output a message to the browser or CLI
 	 *
-	 * @param string  $message
+	 * @param string $message
 	 */
 	public function output($message) {
-		if(PHP_SAPI === 'cli') {
+		if($this->quiet) return;
+		if(Director::is_cli()) {
 			echo $message.PHP_EOL;
 		} else {
-			echo $message.'<br>'.PHP_EOL;
+			echo Convert::raw2xml($message).'<br />'.PHP_EOL;
 		}
 	}
 }
---
+++ tests/CronTaskControllerTest.php
@@ -0,0 +1,100 @@
+<?php
+
+class CronTaskControllerTest extends SapphireTest {
+
+	public function setUp() {
+		parent::setUp();
+		CronTaskTest_TestCron::$times_run = 0;
+	}
+
+	/**
+	 * Tests CronTaskController::isTaskDue
+	 */
+	public function testIsTaskDue() {
+		$runner = CronTaskController::create();
+		$task = new CronTaskTest_TestCron();
+		$cron = Cron\CronExpression::factory($task->getSchedule());
+
+		// Assuming first run, match the exact time (seconds are ignored)
+		SS_Datetime::set_mock_now('2010-06-20 13:00:10');
+		$this->assertTrue($runner->isTaskDue($task, $cron));
+
+		// Assume first run, do not match time just before or just after schedule
+		SS_Datetime::set_mock_now('2010-06-20 13:01:10');
+		$this->assertFalse($runner->isTaskDue($task, $cron));
+		SS_Datetime::set_mock_now('2010-06-20 12:59:50');
+		$this->assertFalse($runner->isTaskDue($task, $cron));
+
+		// Mock a run and test that subsequent runs are properly scheduled
+		SS_Datetime::set_mock_now('2010-06-20 13:30:10');
+		CronTaskStatus::update_status('CronTaskTest_TestCron', true);
+
+		// Job prior to next hour mark should not run
+		SS_Datetime::set_mock_now('2010-06-20 13:40:00');
+		$this->assertFalse($runner->isTaskDue($task, $cron));
+
+		// Jobs just after the next hour mark should run
+		SS_Datetime::set_mock_now('2010-06-20 14:10:00');
+		$this->assertTrue($runner->isTaskDue($task, $cron));
+
+		// Jobs somehow delayed a whole day should be run
+		SS_Datetime::set_mock_now('2010-06-21 13:40:00');
+		$this->assertTrue($runner->isTaskDue($task, $cron));
+	}
+
+	/**
+	 * Test CronTaskController::runTask
+	 */
+	public function testRunTask() {
+		$runner = CronTaskController::create();
+		$runner->setQuiet(true);
+		$task = new CronTaskTest_TestCron();
+
+		// Assuming first run, match the exact time (seconds are ignored)
+		$this->assertEquals(0, CronTaskTest_TestCron::$times_run);
+		SS_Datetime::set_mock_now('2010-06-20 13:00:10');
+		$runner->runTask($task);
+		$this->assertEquals(1, CronTaskTest_TestCron::$times_run);
+
+		// Test that re-requsting the task in the same minute do not retrigger another run
+		SS_Datetime::set_mock_now('2010-06-20 13:00:40');
+		$runner->runTask($task);
+		$this->assertEquals(1, CronTaskTest_TestCron::$times_run);
+
+		// Job prior to next hour mark should not run
+		SS_Datetime::set_mock_now('2010-06-20 13:40:00');
+		$runner->runTask($task);
+		$this->assertEquals(1, CronTaskTest_TestCron::$times_run);
+
+		// Jobs just after the next hour mark should run
+		SS_Datetime::set_mock_now('2010-06-20 14:10:00');
+		$runner->runTask($task);
+		$this->assertEquals(2, CronTaskTest_TestCron::$times_run);
+
+		// Jobs run on the exact next expected date should run
+		SS_Datetime::set_mock_now('2010-06-20 15:00:00');
+		$runner->runTask($task);
+		$this->assertEquals(3, CronTaskTest_TestCron::$times_run);
+
+		// Jobs somehow delayed a whole day should be run
+		SS_Datetime::set_mock_now('2010-06-21 13:40:00');
+		$runner->runTask($task);
+		$this->assertEquals(4, CronTaskTest_TestCron::$times_run);
+	}
+}
+
+
+class CronTaskTest_TestCron implements TestOnly, CronTask {
+
+	public static $times_run = 0;
+
+	public function getSchedule() {
+		// Use hourly schedule
+		return '0 * * * *';
+	}
+
+	public function process() {
+		++self::$times_run;
+	}
+
+}
